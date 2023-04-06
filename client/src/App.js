2b06a20a6deed88e86c6be198027b1d9c7a083d2
dbswp <dbswp980427@gmail.com>
2023-04-06 23:24:17 +0900

    googlemap

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Router from './Router';
-import Header from './routes/Header';
+import "./App.css";
+import Router from "./Router";
+import GoogleMap from "./routes/GoogleMap";
+import Header from "./routes/Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+        <GoogleMap />
         <Router />
       </header>
     </div>
