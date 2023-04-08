f0f891f5f3b9ebd89baaa971e46b24d36e43d374
dbswp <dbswp980427@gmail.com>
2023-04-08 23:18:12 +0900

    logout 추가

--- server/controller/fetchData.js
+++ server/controller/fetchData.js
@@ -1,18 +1,19 @@
-const { parseString } = require('xml2js');
+const { parseString } = require("xml2js");
 
 const { DATA_API_KEY } = process.env;
 const AREA_END_POINT = `http://openapi.seoul.go.kr:8088/${DATA_API_KEY}/xml/citydata/1/5/신촌·이대역`;
 
 async function fetchData() {
   let model = {
-    area_name: '',
+    area_name: "",
     live_data: {},
+    live_weather: {},
   };
 
   const resolve = await fetch(AREA_END_POINT, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/xml',
+      "Content-Type": "application/xml",
     },
   });
 
@@ -24,22 +25,23 @@ async function fetchData() {
     } else {
       //KT의 실시간 인구밀도 데이터
       let liveData =
-        result['SeoulRtd.citydata']['CITYDATA'][0].LIVE_PPLTN_STTS[0]
+        result["SeoulRtd.citydata"]["CITYDATA"][0].LIVE_PPLTN_STTS[0]
           .LIVE_PPLTN_STTS[0];
       //지역 이름
-      let areaName = result['SeoulRtd.citydata']['CITYDATA'][0].AREA_NM;
+      let areaName = result["SeoulRtd.citydata"]["CITYDATA"][0].AREA_NM;
       //당일 전체적인 날씨
       let dayWeather =
-        result['SeoulRtd.citydata']['CITYDATA'][0].WEATHER_STTS[0]
-          .WEATHER_STTS[0];
+        result["SeoulRtd.citydata"]["CITYDATA"][0].WEATHER_STTS[0]
+          .WEATHER_STTS[0].PCP_MSG[0];
       //당일 시간별 날씨
       let timeWeather =
-        result['SeoulRtd.citydata']['CITYDATA'][0].WEATHER_STTS[0]
+        result["SeoulRtd.citydata"]["CITYDATA"][0].WEATHER_STTS[0]
           .WEATHER_STTS[0].FCST24HOURS[0].FCST24HOURS;
 
       model = {
         area_name: areaName[0],
         live_data: liveData,
+        live_weather: dayWeather,
       };
     }
   });
--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,7 +1,7 @@
-const mongooseConnect = require('./mongooseConnect');
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
-const { json } = require('body-parser');
+const mongooseConnect = require("./mongooseConnect");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const { json } = require("body-parser");
 
 const { ACCESS_SECRET, REFRESH_SECRET, KAKAO_API_KEY, KAKAO_REDIRECT_URI } =
   process.env;
@@ -14,11 +14,11 @@ const registerUser = async (req, res) => {
   console.log(req.body);
   try {
     await User.create(req.body);
-    res.status(200).json({ text: '회원가입 성공!!' });
-    console.log('회원가입 성공!');
+    res.status(200).json({ text: "회원가입 성공!!" });
+    console.log("회원가입 성공!");
   } catch (err) {
     console.error(err);
-    res.status(500).json({ text: '회원가입 실패' });
+    res.status(500).json({ text: "회원가입 실패" });
   }
 };
 
@@ -27,15 +27,15 @@ const loginUser = async (req, res) => {
   try {
     const duplicatedUser = await User.findOne({ id });
     if (duplicatedUser) {
-      console.log('db에서 아이디 대조까지는 성공');
+      console.log("db에서 아이디 대조까지는 성공");
     }
     if (!duplicatedUser) {
-      return res.status(400).json({ text: '없는 아이디임..' });
+      return res.status(400).json({ text: "없는 아이디임.." });
     }
     if (duplicatedUser.password !== req.body.password) {
-      return res.status(400).json({ text: '비밀번호 틀림!' });
+      return res.status(400).json({ text: "비밀번호 틀림!" });
     }
-    console.log(duplicatedUser);
+    // console.log(duplicatedUser);
 
     // accesstoken 발급
     const accessToken = jwt.sign(
@@ -45,7 +45,7 @@ const loginUser = async (req, res) => {
       ACCESS_SECRET,
       {
         expiresIn: 1000 * 60,
-        issuer: 'About Tech',
+        issuer: "About Tech",
       }
     );
 
@@ -56,17 +56,17 @@ const loginUser = async (req, res) => {
       },
       REFRESH_SECRET,
       {
-        expiresIn: '24h',
-        issuer: 'About Tech',
+        expiresIn: "24h",
+        issuer: "About Tech",
       }
     );
 
     // 쿠키에 담아서 전송
-    res.cookie('accessToken', accessToken, {
+    res.cookie("accessToken", accessToken, {
       secure: false,
       httpOnly: false,
     });
-    res.cookie('refreshToken', refreshToken, {
+    res.cookie("refreshToken", refreshToken, {
       secure: false,
       httpOnly: false,
     });
@@ -77,37 +77,45 @@ const loginUser = async (req, res) => {
 
     //로그인 성공시 프론트단에서 화면이 변화되는 처리를 해줘야할듯!!!
 
-    res.status(200).json({ text: '로그인 성공' });
+    res.status(200).json({ text: "로그인 성공" });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ text: '로그인 오류' });
+    res.status(500).json({ text: "로그인 오류" });
   }
 };
 
 const kakaoLoginUser = (req, res) => {
   const KAKAO_CODE = req.body.code;
-  // console.log(KAKAO_CODE);
+  console.log(KAKAO_CODE);
   try {
     fetch(
       `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${KAKAO_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&code=${KAKAO_CODE}`,
       {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'x-www-form-urlencoded;charset=utf-8',
+          "Content-Type": "x-www-form-urlencoded;charset=utf-8",
         },
       }
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(
-          '카카오 엑세스 토큰 발급 성공!!   토큰:',
-          data.access_token
-        );
+        console.log("카카오 엑세스 토큰 발급 성공!!   토큰:", KAKAO_CODE);
+        // accesstoken 발급
+        // const accessToken = jwt.sign(
+        //   {
+        //     id: data.id,
+        //   },
+        //   ACCESS_SECRET,
+        //   {
+        //     expiresIn: 1000 * 60,
+        //     issuer: "About Tech",
+        //   }
+        // );
       });
-    res.status(200).json('엑세스 토큰 받기 성공!');
+    res.status(200).json("엑세스 토큰 받기 성공!");
   } catch (err) {
     console.error(err);
-    res.status(500).json('엑세스 토큰 받기 실패!!');
+    res.status(500).json("엑세스 토큰 받기 실패!!");
   }
 };
 
@@ -147,16 +155,16 @@ const refreshtoken = async (req, res) => {
       },
       ACCESS_SECRET,
       {
-        expiresIn: '1m',
-        issuer: 'About Tech',
+        expiresIn: "1m",
+        issuer: "About Tech",
       }
     );
 
-    res.cookie('accessToken', accessToken, {
+    res.cookie("accessToken", accessToken, {
       secure: false,
       httpOnly: true,
     });
-    res.status(200).json('Access Token Recreated');
+    res.status(200).json("Access Token Recreated");
     console.log(userData);
   } catch (err) {
     res.status(500).json(err);
@@ -165,6 +173,15 @@ const refreshtoken = async (req, res) => {
 
 const loginSuccess = (req, res) => {};
 
+const logout = (req, res) => {
+  try {
+    res.cookie("accessToken", "");
+    res.status(200).json("Logout Success");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 module.exports = {
   loginUser,
   accesstoken,
@@ -172,4 +189,5 @@ module.exports = {
   loginSuccess,
   registerUser,
   kakaoLoginUser,
+  logout,
 };
