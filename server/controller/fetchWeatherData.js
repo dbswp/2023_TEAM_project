4028090dfadbb5dbbf26d60ea460cb4b20feaf30
JUMTBOX <yjey12@naver.com>
2023-04-08 19:05:43 +0900

    #9 feat.데이터 스키마 작성 및 색인, 데이터 요청 주소 end point 수정 완료

--- server/controller/fetchWeatherData.js
+++ server/controller/fetchWeatherData.js
@@ -1,13 +1,19 @@
 const { parseString } = require('xml2js');
 
 const { DATA_API_KEY } = process.env;
+// const AREA_END_POINT = `http://openapi.seoul.go.kr:8088/${DATA_API_KEY}/xml/citydata/1/5/신촌·이대역`;
 
-async function fetchData(END_POINT) {
+async function fetchWeatherData(END_POINT) {
   const AREA_END_POINT = `http://openapi.seoul.go.kr:8088/${DATA_API_KEY}/xml/citydata/1/5/${END_POINT}`;
 
-  let model = {
-    area_name: '',
-    live_data: {},
+  let weatherModel = {
+    temperature: '',
+    sen_temperature: '',
+    min_temperature: '',
+    max_temperature: '',
+    pcp_msg: '',
+    air_idx: '',
+    fcst_24hours: {},
   };
 
   const resolve = await fetch(AREA_END_POINT, {
@@ -23,12 +29,6 @@ async function fetchData(END_POINT) {
     if (err) {
       console.error(err);
     } else {
-      //KT의 실시간 인구밀도 데이터
-      let liveData =
-        result['SeoulRtd.citydata']['CITYDATA'][0].LIVE_PPLTN_STTS[0]
-          .LIVE_PPLTN_STTS[0];
-      //지역 이름
-      let areaName = result['SeoulRtd.citydata']['CITYDATA'][0].AREA_NM;
       //당일 전체적인 날씨
       let dayWeather =
         result['SeoulRtd.citydata']['CITYDATA'][0].WEATHER_STTS[0]
@@ -36,15 +36,20 @@ async function fetchData(END_POINT) {
       //당일 시간별 날씨
       let timeWeather =
         result['SeoulRtd.citydata']['CITYDATA'][0].WEATHER_STTS[0]
-          .WEATHER_STTS[0].FCST24HOURS[0].FCST24HOURS;
+          .WEATHER_STTS[0].FCST24HOURS[0];
 
-      model = {
-        area_name: areaName[0],
-        live_data: liveData,
+      weatherModel = {
+        temperature: dayWeather.TEMP[0],
+        sen_temperature: dayWeather.SENSIBLE_TEMP[0],
+        min_temperature: dayWeather.MIN_TEMP[0],
+        max_temperature: dayWeather.MAX_TEMP[0],
+        pcp_msg: dayWeather.PCP_MSG[0],
+        air_idx: dayWeather.AIR_IDX[0],
+        fcst_24hours: timeWeather,
       };
     }
   });
-  return model;
+  return weatherModel;
 }
 
-module.exports = fetchData;
+module.exports = fetchWeatherData;
