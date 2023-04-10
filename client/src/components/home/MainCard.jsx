729ada7246a9d028f5665acc60e6654877935633
지영 <toto.joung@gmail.com>
2023-04-10 11:01:36 +0900

    #18 feat.home에 maintabs버튼 추가

--- client/src/components/home/MainCard.jsx
+++ client/src/components/home/MainCard.jsx
@@ -8,20 +8,18 @@ export default function MainCard() {
 
   return (
     <>
-      <div className="container">
-        <div className="row">
-          {arr.map((name, index) => {
-            return (
-              <Card
-                onClick={() => {
-                  navigate("/blog");
-                }}
-                name={name}
-                key={index}
-              />
-            );
-          })}
-        </div>
+      <div className="row">
+        {arr.map((name, index) => {
+          return (
+            <Card
+              onClick={() => {
+                navigate("/blog");
+              }}
+              name={name}
+              key={index}
+            />
+          );
+        })}
       </div>
     </>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import MainBanner from "../components/home/MainBanner";
 import MainCard from "../components/home/MainCard";
+import MainTabsBtn from "../components/home/MainTabsBtn";
 
 export default function Home() {
   return (
     <div>
       <MainBanner />
-      <MainCard />
+      <MainTabsBtn />
     </div>
   );
 }
