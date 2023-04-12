edcbec5900bd4bea057aff503e9823ba32d50fea
JUMTBOX <yjey12@naver.com>
2023-04-12 21:04:53 +0900

    #26 feat. SMS 푸시 알림 기능완성

--- client/src/components/home/MainTabsButton.jsx
+++ client/src/components/home/MainTabsButton.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useRef, useState } from "react";
-import "../../styles/main-tabs-button.scss";
+import React, { useEffect, useRef, useState } from 'react';
+import '../../styles/main-tabs-button.scss';
 
-import Card from "./Card";
-import axios from "axios";
+import Card from './Card';
+import axios from 'axios';
 
 export default function MainTabsButton() {
   const getData = async () => {
-    const res = await axios.get("http://localhost:4000/nameData");
-    if (res.status !== 200) alert("데이터 수신 실패");
+    const res = await axios.get('http://localhost:4000/nameData');
+    if (res.status !== 200) alert('데이터 수신 실패');
+    console.log(res.data.Arr);
     // 전체 분류 데이터
     const allDataBase = res.data.Arr;
-    // 첫번째 전체 데이터
+    // // 첫번째 전체 데이터
     const firstNameData = res.data.Arr[0].name;
     const firstImgData = res.data.Arr[0].img;
 
@@ -27,6 +28,7 @@ export default function MainTabsButton() {
   const [allData, setAllData] = useState();
   const [allImgData, setAllImgData] = useState();
   const [toggleState, setToggleState] = useState(0);
+
   const toggleTab = (index) => {
     setToggleState(index);
   };
@@ -38,7 +40,7 @@ export default function MainTabsButton() {
           <ul className="tab-list">
             <li className="tab-item">
               <button
-                className={toggleState === 0 ? "nav-link active" : "nav-link"}
+                className={toggleState === 0 ? 'nav-link active' : 'nav-link'}
                 onClick={() => {
                   toggleTab(0);
                 }}
@@ -48,7 +50,7 @@ export default function MainTabsButton() {
             </li>
             <li className="tab-item">
               <button
-                className={toggleState === 1 ? "nav-link active" : "nav-link"}
+                className={toggleState === 1 ? 'nav-link active' : 'nav-link'}
                 onClick={() => {
                   toggleTab(1);
                 }}
@@ -58,7 +60,7 @@ export default function MainTabsButton() {
             </li>
             <li className="tab-item">
               <button
-                className={toggleState === 2 ? "nav-link active" : "nav-link"}
+                className={toggleState === 2 ? 'nav-link active' : 'nav-link'}
                 onClick={() => {
                   toggleTab(2);
                 }}
@@ -68,7 +70,7 @@ export default function MainTabsButton() {
             </li>
             <li className="tab-item">
               <button
-                className={toggleState === 3 ? "nav-link active" : "nav-link"}
+                className={toggleState === 3 ? 'nav-link active' : 'nav-link'}
                 onClick={() => {
                   toggleTab(3);
                 }}
@@ -78,42 +80,42 @@ export default function MainTabsButton() {
             </li>
           </ul>
         </div>
-        <div className="row" style={{ height: "500px" }}>
+        <div className="row" style={{ height: '500px' }}>
           <div className="tab-contents-wrap">
-            <div className={toggleState === 0 ? "tab-content1" : "tab-content"}>
+            <div className={toggleState === 0 ? 'tab-content1' : 'tab-content'}>
               {allData?.map((el, idx) => {
                 return <Card name={el} key={idx} img={allImgData[idx]} />;
               })}
             </div>
-            <div className={toggleState === 1 ? "tab-content2" : "tab-content"}>
+            <div className={toggleState === 1 ? 'tab-content2' : 'tab-content'}>
               {allDataBase[toggleState]?.name?.map((el, idx) => {
                 return (
                   <Card
                     key={idx}
                     name={el}
-                    img={allDataBase[toggleState].img[idx]}
+                    img={allDataBase[toggleState]?.img[idx]}
                   />
                 );
               })}
             </div>
-            <div className={toggleState === 2 ? "tab-content3" : "tab-content"}>
+            <div className={toggleState === 2 ? 'tab-content3' : 'tab-content'}>
               {allDataBase[toggleState]?.name?.map((el, idx) => {
                 return (
                   <Card
                     key={idx}
                     name={el}
-                    img={allDataBase[toggleState].img[idx]}
+                    img={allDataBase[toggleState]?.img[idx]}
                   />
                 );
               })}
             </div>
-            <div className={toggleState === 3 ? "tab-content4" : "tab-content"}>
+            <div className={toggleState === 3 ? 'tab-content4' : 'tab-content'}>
               {allDataBase[toggleState]?.name?.map((el, idx) => {
                 return (
                   <Card
                     key={idx}
                     name={el}
-                    img={allDataBase[toggleState].img[idx]}
+                    img={allDataBase[toggleState]?.img[idx]}
                   />
                 );
               })}
