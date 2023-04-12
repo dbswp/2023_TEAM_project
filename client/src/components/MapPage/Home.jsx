6b162b7cb1b204b78868f078b79d31d4a9c8ec26
kyehwan <clon0306@gmail.com>
2023-04-12 13:34:42 +0900

    feature#19 사이드바 백엔드 데이터 받기 완료

--- client/src/components/MapPage/Home.jsx
+++ client/src/components/MapPage/Home.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import { BiMenu } from "react-icons/bi";
+import { BiMap, BiMenu } from "react-icons/bi";
 import { useGlobalContext } from "./Context";
 import styles from "../../styles/mp-home.scss";
+import { TiWeatherSunny } from "react-icons/ti";
+import { IoIosPeople } from "react-icons/io";
 
 const Home = () => {
   const { openSidebar } = useGlobalContext();
@@ -11,6 +13,12 @@ const Home = () => {
       <div className="wrap-container">
         <button className="sidebar-toggle" onClick={openSidebar}>
           <BiMenu />
+        </button>
+        <button className="sidebar-location" onClick={openSidebar}>
+          <BiMap />
+        </button>
+        <button className="sidebar-population" onClick={openSidebar}>
+          <IoIosPeople />
         </button>
       </div>
     </main>
