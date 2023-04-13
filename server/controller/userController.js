e4a7ba70944fa78ab89abb93493949f7f6c30b2e
JUMTBOX <yjey12@naver.com>
2023-04-13 22:31:45 +0900

    fix. 중복코드 삭제

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -17,11 +17,11 @@ const registerUser = async (req, res) => {
   console.log(req.body);
   try {
     await User.create(req.body);
-    res.status(200).json({ text: "회원가입 성공!!" });
-    console.log("회원가입 성공!");
+    res.status(200).json({ text: '회원가입 성공!!' });
+    console.log('회원가입 성공!');
   } catch (err) {
     console.error(err);
-    res.status(500).json({ text: "회원가입 실패" });
+    res.status(500).json({ text: '회원가입 실패' });
   }
 };
 
@@ -30,13 +30,13 @@ const loginUser = async (req, res) => {
   try {
     const duplicatedUser = await User.findOne({ email });
     if (duplicatedUser) {
-      console.log("db에서 아이디 대조까지는 성공");
+      console.log('db에서 아이디 대조까지는 성공');
     }
     if (!duplicatedUser) {
-      return res.status(400).json({ text: "없는 이메일입니다." });
+      return res.status(400).json({ text: '없는 이메일입니다.' });
     }
     if (duplicatedUser.password !== req.body.password) {
-      return res.status(400).json({ text: "비밀번호 틀림" });
+      return res.status(400).json({ text: '비밀번호 틀림' });
     }
 
     // 유저가 로그인 하면 true로 바꾸어줌
@@ -54,7 +54,7 @@ const loginUser = async (req, res) => {
       ACCESS_SECRET,
       {
         expiresIn: 1000 * 60,
-        issuer: "About Tech",
+        issuer: 'About Tech',
       }
     );
 
@@ -67,25 +67,25 @@ const loginUser = async (req, res) => {
       },
       REFRESH_SECRET,
       {
-        expiresIn: "24h",
-        issuer: "About Tech",
+        expiresIn: '24h',
+        issuer: 'About Tech',
       }
     );
 
     // 쿠키에 담아서 전송
-    res.cookie("accessToken", accessToken, {
+    res.cookie('accessToken', accessToken, {
       secure: false,
       httpOnly: false,
     });
-    res.cookie("refreshToken", refreshToken, {
+    res.cookie('refreshToken', refreshToken, {
       secure: false,
       httpOnly: false,
     });
 
-    res.status(200).json({ text: "로그인 성공" });
+    res.status(200).json({ text: '로그인 성공' });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ text: "로그인 오류" });
+    res.status(500).json({ text: '로그인 오류' });
   }
 };
 
@@ -94,23 +94,9 @@ const kakaoLoginUser = async (req, res) => {
   const KAKAO_CODE = req.body.kakao_access_token;
 
   try {
-    // 카카오 인가토큰을 가지고 엑세스 토큰을 요청
-    const getKakaoAccessToken = await fetch(
-      `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${KAKAO_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&code=${KAKAO_CODE}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "x-www-form-urlencoded;charset=utf-8",
-        },
-      }
-    );
-    const data = await getKakaoAccessToken.json();
-    kakao_access_token = data.access_token;
-    console.log("카카오 엑세스 토큰 발급 성공!!   토큰:", data.access_token);
-
     //카카오 엑세스 토큰을 사용하여 사용자 정보에 접근!
     const userResponese = await fetch(`https://kapi.kakao.com/v2/user/me`, {
-      method: "POST",
+      method: 'POST',
       headers: {
         Authorization: `Bearer ${KAKAO_CODE}`,
         'Content-type': 'application/x-www-form-urlencoded',
@@ -130,17 +116,17 @@ const kakaoLoginUser = async (req, res) => {
         ACCESS_SECRET,
         {
           expiresIn: 1000 * 60,
-          issuer: "About Tech",
+          issuer: 'About Tech',
         }
       );
       // 쿠키에 담아서 전송
-      res.cookie("kakaoAccessToken", kakaoAccessToken, {
+      res.cookie('kakaoAccessToken', kakaoAccessToken, {
         secure: false,
         httpOnly: false,
       });
       // console.log(kakaoAccessToken);
     }
-    res.status(200).json("엑세스 토큰 받기 성공!");
+    res.status(200).json('엑세스 토큰 받기 성공!');
   } catch (err) {
     console.error(err);
   }
@@ -182,16 +168,16 @@ const refreshtoken = async (req, res) => {
       },
       ACCESS_SECRET,
       {
-        expiresIn: "1m",
-        issuer: "About Tech",
+        expiresIn: '1m',
+        issuer: 'About Tech',
       }
     );
 
-    res.cookie("accessToken", accessToken, {
+    res.cookie('accessToken', accessToken, {
       secure: false,
       httpOnly: true,
     });
-    res.status(200).json("Access Token Recreated");
+    res.status(200).json('Access Token Recreated');
     console.log(userData);
   } catch (err) {
     res.status(500).json(err);
@@ -203,8 +189,8 @@ const loginSuccess = (req, res) => {};
 const logout = (req, res) => {
   console.log('들어오나?');
   try {
-    res.cookie("accessToken", " ");
-    res.status(200).json("Logout Success");
+    res.cookie('accessToken', ' ');
+    res.status(200).json('Logout Success');
   } catch (error) {
     res.status(500).json(error);
   }
