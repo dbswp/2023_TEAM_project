7450e1e3950e86bf33e8dbfce9e4a6d058ff4314
dbswp <dbswp980427@gmail.com>
2023-04-13 09:23:32 +0900

    feature#22 카드 정보 저장

--- client/src/components/home/Card.jsx
+++ client/src/components/home/Card.jsx
@@ -6,11 +6,11 @@ const { DATA_API_KEY } = process.env;
 export default function Card({ name, img }) {
   const divName = async () => {
     try {
-      const res = await axios.post("http://localhost:4000/data/getdata", {
+      const res = await axios.post("http://localhost:4000/data/getdata/", {
         END_POINT: name,
       });
       // 데이터 처리 로직
-      window.location.href = `http://openapi.seoul.go.kr:8088/${DATA_API_KEY}/xml/citydata/1/5/${res}`; // 이동할 URL 주소
+      window.location.href = `http://localhost:3000/blog/${res}`; // 이동할 URL 주소
     } catch (error) {
       console.error(error);
     }
