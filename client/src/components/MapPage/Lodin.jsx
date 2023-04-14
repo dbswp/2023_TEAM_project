2bc2b1cb4d1eaaeeb416191c45e71fbd36871992
dbswp <dbswp980427@gmail.com>
2023-04-14 19:07:14 +0900

    feat[#22] : 모바일 반응형 완료, 헤더 css 오류나는거 수정해야함

--- client/src/components/MapPage/Lodin.jsx
+++ client/src/components/MapPage/Lodin.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Spinner from "../../assets/spinner.gif";
+import Spinner from "../../assets/Spinner.gif";
 import "../../styles/loding.scss";
 
 function Loding() {
