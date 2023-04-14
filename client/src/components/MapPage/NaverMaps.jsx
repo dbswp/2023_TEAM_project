73f7592269a034473d474850095eb3109e712d76
kyehwan <clon0306@gmail.com>
2023-04-14 10:04:28 +0900

    feature#63 지도 위 다수 마커 표시

--- client/src/components/MapPage/NaverMaps.jsx
+++ client/src/components/MapPage/NaverMaps.jsx
@@ -1,74 +1,92 @@
-import axios from 'axios';
+import axios from "axios";
+import { useEffect, useState } from "react";
 import {
   Container as MapDiv,
   NaverMap,
   Marker,
   useNavermaps,
-} from 'react-naver-maps';
-import '../../styles/mp-sidebar.scss';
+  RenderAfterNavermapsLoaded,
+  Polygon,
+  Polyline,
+} from "react-naver-maps";
+import "../../styles/mp-sidebar.scss";
+import Papa from "papaparse";
 
 export default function NaverMaps() {
-  const navermaps = useNavermaps();
-  const point_latitude = localStorage.getItem('latitude');
-  const point_longitude = localStorage.getItem('longitude');
+  const [coordinates, setCoordinates] = useState([]);
 
-  //인구 밀집도가 일정 레벨이상이 되면 동작시킬 함수(로직 작성 필요!)
-  const sendKakaoAccessToken = async () => {
-    //알림 메시지 기능에 사용할 카카오 엑세스 토큰을 로컬스토리지에서 가져와서 변수에 저장
-    const kakao_access_token = window.localStorage.getItem('kakaoAccessToken');
-
-    //요청을 카카오 엑세스 토큰과 함께 알림기능 라우터에 전달
-    await axios.post('http://localhost:4000/push', {
-      kakao_access_token,
+  useEffect(() => {
+    Papa.parse("/data/coordinates.csv", {
+      download: true,
+      header: true,
+      complete: function (results) {
+        setCoordinates(results.data);
+      },
     });
-  };
+  }, []);
 
-  const positions = [
-    {
-      title: '카카오',
-      latlng: { lat: 33.450705, lng: 126.570677 },
-    },
-    {
-      title: '생태연못',
-      latlng: { lat: 33.450936, lng: 126.569477 },
-    },
-    {
-      title: '텃밭',
-      latlng: { lat: 33.450879, lng: 126.56994 },
-    },
-    {
-      title: '근린공원',
-      latlng: { lat: 33.451393, lng: 126.570738 },
-    },
-  ];
+  const navermaps = useNavermaps();
+  const point_latitude = localStorage.getItem("latitude");
+  const point_longitude = localStorage.getItem("longitude");
 
   return (
     <>
-      {
-        <NaverMap
-          defaultCenter={new navermaps.LatLng(point_latitude, point_longitude)}
-          defaultZoom={15}
-        >
-          {/* {marker_location?.map((el, idx) => {
-            <Marker key={idx} Position={new navermaps.LatLng(el)} />;
-          })} */}
-
-          {/* 마커표시 */}
-          {/* <Marker
-            key={`${positions.title}-${positions.latlng}`}
-            position={new navermaps.LatLng`${positions.latlng}`()} // 마커를 표시할 위치
-            title={positions.title} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-          /> */}
+      <NaverMap
+        defaultCenter={new navermaps.LatLng(point_latitude, point_longitude)}
+        defaultZoom={15}
+      >
+        {/* 마커표시 */}
+        {coordinates.map((coordinate, index) => (
           <Marker
-            key={1}
-            position={new navermaps.LatLng(37.551229, 126.988205)}
-            animation={2}
+            key={index}
+            position={
+              new navermaps.LatLng(coordinate.latitude, coordinate.longitude)
+            }
+            animation={navermaps.Animation.NONE}
             onClick={() => {
-              alert('여기는 N서울타워입니다.');
+              alert(`${coordinate.name}.`);
             }}
           />
-        </NaverMap>
-      }
+        ))}
+
+        <Polyline
+          clickable={true}
+          strokeColor="blue"
+          strokeStyle="solid"
+          strokeWeight={1}
+          path={[
+            new navermaps.LatLng(37.359924641705476, 127.1148204803467),
+            new navermaps.LatLng(37.36343797188166, 127.11486339569092),
+            new navermaps.LatLng(37.368520071054576, 127.11473464965819),
+            new navermaps.LatLng(37.3685882848096, 127.1088123321533),
+            new navermaps.LatLng(37.37295383612657, 127.10876941680907),
+            new navermaps.LatLng(37.38001321351567, 127.11851119995116),
+            new navermaps.LatLng(37.378546827477855, 127.11984157562254),
+            new navermaps.LatLng(37.376637072444105, 127.12052822113036),
+            new navermaps.LatLng(37.37530703574853, 127.12190151214598),
+            new navermaps.LatLng(37.371657839593894, 127.11645126342773),
+            new navermaps.LatLng(37.36855417793982, 127.1207857131958),
+          ]}
+        />
+        <Polygon
+          fillColor="salmon"
+          fillOpacity={0.35}
+          clickable={true}
+          paths={[
+            new navermaps.LatLng(37.359924641705476, 127.1148204803467),
+            new navermaps.LatLng(37.36343797188166, 127.11486339569092),
+            new navermaps.LatLng(37.368520071054576, 127.11473464965819),
+            new navermaps.LatLng(37.3685882848096, 127.1088123321533),
+            new navermaps.LatLng(37.37295383612657, 127.10876941680907),
+            new navermaps.LatLng(37.38001321351567, 127.11851119995116),
+            new navermaps.LatLng(37.378546827477855, 127.11984157562254),
+            new navermaps.LatLng(37.376637072444105, 127.12052822113036),
+            new navermaps.LatLng(37.37530703574853, 127.12190151214598),
+            new navermaps.LatLng(37.371657839593894, 127.11645126342773),
+            new navermaps.LatLng(37.36855417793982, 127.1207857131958),
+          ]}
+        />
+      </NaverMap>
     </>
   );
 }
