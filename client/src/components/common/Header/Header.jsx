040b3e873a20512f0ea811bbcb19d865ed8ef022
dbswp <dbswp980427@gmail.com>
2023-04-14 19:48:19 +0900

    feature[#22] : 로그아웃 버튼 삭제

--- client/src/components/common/Header/Header.jsx
+++ client/src/components/common/Header/Header.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import Logo from '../../../assets/Logo.gif';
-import { kakaoLogoutUrl } from '../../../kakaoLoginData';
-import '../../../styles/header.scss';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../../../assets/Logo.gif";
+import { kakaoLogoutUrl } from "../../../kakaoLoginData";
+import "../../../styles/header.scss";
 
-window.addEventListener('scroll', () => {
-  const headerWrap = document.querySelector('.headerWrap');
+window.addEventListener("scroll", () => {
+  const headerWrap = document.querySelector(".headerWrap");
 
   let scrollLocation = document.documentElement.scrollTop; // 현재 스크롤바 위치
 
   if (scrollLocation > 50 && scrollLocation < 860) {
-    headerWrap.classList.add('scrollHeader');
+    headerWrap.classList.add("scrollHeader");
   } else if (scrollLocation >= 860) {
-    headerWrap.classList.remove('scrollHeader');
+    headerWrap.classList.remove("scrollHeader");
   } else {
-    headerWrap.classList.remove('scrollHeader');
+    headerWrap.classList.remove("scrollHeader");
   }
 });
 
@@ -25,8 +25,8 @@ export default function Header() {
 
   const logout = () => {
     // 로그아웃 할 때 쿠키삭제하는 방법 (옛날에 만든 토큰이라고 지정해서 없애는 방법)
-    document.cookie = 'refreshToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    document.cookie = 'accessToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = "refreshToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    document.cookie = "accessToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
     window.location.reload();
   };
   return (
