658a1f52288ba16d222cb6f4128e0108d407e888
dbswp <dbswp980427@gmail.com>
2023-04-17 11:19:31 +0900

    feat[#22] : 회워가입 로그인 다시하는 중

--- server/routes/boardPrint.js
+++ server/routes/boardPrint.js
@@ -5,19 +5,19 @@ const { ACCESS_SECRET } = process.env;
 
 const { getArticles, writeArticle } = require("../controller/boardController");
 
-const checkToken = (req, res, next) => {
-  const token = req.cookies.accessToken;
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-  try {
-    const decoded = jwt.verify(token, ACCESS_SECRET);
-    req.email = decoded.email;
-    next();
-  } catch (err) {
-    return res.status(403).json({ message: "Invalid token" });
-  }
-};
+// const checkToken = (req, res, next) => {
+//   const token = req.cookies.accessToken;
+//   if (!token) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
+//   try {
+//     const decoded = jwt.verify(token, ACCESS_SECRET);
+//     req.email = decoded.email;
+//     next();
+//   } catch (err) {
+//     return res.status(403).json({ message: "Invalid token" });
+//   }
+// };
 
 router.get("/", getArticles);
 router.post("/write", writeArticle);
