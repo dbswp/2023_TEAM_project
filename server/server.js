7e26d88e02a5f5416e9b7fc792cdbb3bcf1fdac5
dbswp <dbswp980427@gmail.com>
2023-04-19 04:59:55 +0900

    설정

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use("/nameData", seoulDataRouter);
 app.use("/push", pushAlarmRouter);
 app.use("/board", boardRouter);
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
